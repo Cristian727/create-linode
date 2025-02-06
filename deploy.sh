b15e4f5386b7712edd2001c7f0ff77a67bf750d0
Cristian727 <cristian.gonzalez7@hotmail.com>
2025-02-06 12:52:42 +0100

    deploy

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "conectando al servidor remoto..."
+
+apt update
+
+apt install npm nodejs certbot 
+
+npm install
+
+#generar certificados ssl usando certbot
+certbot certonly --standalone -d dev5.cyberbunny.online --non-interactive --agree-tos -m cristian727@gmail.com
+
+#copiar los certificados generados a la ubicación essperada por tu apllicación
+cp /etc/letsencrypt/live/dev5.cyberbunny.online/fullchain.pem
+cp /etc/letsencrypt/live/dev5.cyberbunny.online/privkey.pem
+
+
+echo "proceso completado"
